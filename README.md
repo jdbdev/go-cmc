c8f48357b6d7f0f6e3aa7bd087c6eee183bc24bd
jdbdev <juliendb.dev@gmail.com>
2025-03-31 23:44:51 -0300

    godotenv library added to access .env variables

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## Project Summary:
 
-This module implements a connection the the Coinmarketcap API to gather current coin information and update the Database on a set time interval <br>
-cmc = Coinmarketcap
+This module implements a connection the the Coinmarketcap API to gather current token/coin information and update the Database on a set time interval <br>
 
-## License
-
+### License
 MIT
--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jdbdev/go-cmc/types"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("test package 'main' build")
+	// test package types integration:
 	a := types.TestTypes("types")
 	fmt.Printf("loading package: %s", a)
+
+	// load variables from .env file
+	godotenv.Load()
+	// test access to .env:
+	fmt.Println(".env variable for TEST is:", os.Getenv("ENV1"))
 	
 }
