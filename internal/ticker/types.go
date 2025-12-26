64480dc1776cd3b6a7aad3372180f762351ad0cc
Julien Boudreau <juliendb.dev@gmail.com>
2025-12-25 20:14:37 -0400

    refactored ticker/types.go to match JSON structure and added pointers to nullable fields from CMC API

--- internal/ticker/types.go
+++ internal/ticker/types.go
@@ -1,33 +1,54 @@
 package ticker
 
-// CoinInfo holds static data and time of last CMC update
-// DB table: coin_info
+// All JSON fields that can be null in CMC response are pointers allowing null values to avoid
+//unmarshalling errors or setting zero values instead of nil.
+// Always check documentation when adding new fields.
+// Always check for nil if trying to dereference a pointer to avoid runtime errors (panic).
+
+// CMCResponse holds the response from the CMC API.
+type CMCResponse struct {
+	Status Status              `json:"status"`
+	Data   map[string]CoinInfo `json:"data"`
+}
+
+// Status holds the response status from CMC API.
+type Status struct {
+	Timestamp    string  `json:"timestamp"`
+	ErrorCode    int     `json:"error_code"`
+	ErrorMessage *string `json:"error_message"`
+	Elapsed      int     `json:"elapsed"`
+	CreditCount  int     `json:"credit_count"`
+	Notice       *string `json:"notice"`
+}
+
+// CoinInfo holds the coin related information from CMC API, including CoinQuote data.
 type CoinInfo struct {
-	ID      int                   `json:"id"`         // static
-	CmcID   int                   `json:"cmc_id"`     // static
-	Name    string                `json:"name"`       // static
-	Symbol  string                `json:"symbol"`     // static
-	Slug    string                `json:"slug"`       // static
-	Created int                   `json:"created_at"` // dynamic
-	Updated int                   `json:"updated_at"` // dynamic
-	Quotes  map[string]*CoinQuote `json:"quotes"`     // dynamic
+	ID                int     `json:"id"`
+	Name              string  `json:"name"`
+	Symbol            string  `json:"symbol"`
+	Slug              string  `json:"slug"`
+	CirculatingSupply float64 `json:"circulating_supply"`
+	TotalSupply       float64 `json:"total_supply"`
+	InfiniteSupply    bool    `json:"infinite_supply"`
+	MarketCap         float64 `json:"market_cap"`
+	// SelfReportedCirculatingSupply *float64 `json:"self_reported_circulating_supply"`
+	// SelfReportedMarketCap *float64 `json:"self_reported_market_cap"`
+	// TvlRatio *float64 `json:"tvl_ratio"`
+	// Tvl *float64 `json:"tvl"`
+	PercentChange24h float64              `json:"percent_change_24h"`
+	PercentChange7d  float64              `json:"percent_change_7d"`
+	PercentChange30d float64              `json:"percent_change_30d"`
+	PercentChange60d float64              `json:"percent_change_60d"`
+	PercentChange90d float64              `json:"percent_change_90d"`
+	LastUpdated      string               `json:"last_updated"`
+	Quote            map[string]CoinQuote `json:"quote"`
 }
 
-// CoinSupply holds data related to supply
-// DB table: coin_supply
-type CoinSupply struct {
-	CirculatingSupply float64 `json:"circulating_supply"` // dynamic
-	TotalSupply       float64 `json:"total_supply"`       // dynamic
-	MaxSupply         float64 `json:"max_supply"`         // dynamic
-}
-
-// CoinQuote holds dynamic price and volume data
-// DB table: coin_quote
+// CoinQuote holds the quote data for a coin from CMC API. Only uses USD for map key in CoinInfo.
 type CoinQuote struct {
-	Price            float64 `json:"price"`              // dynamic
-	Volume24H        float64 `json:"volume_24h"`         // dynamic
-	MarketCap        float64 `json:"market_cap"`         // dynamic
-	PercentChange1H  float64 `json:"percent_change_1h"`  // dynamic
-	PercentChange24H float64 `json:"percent_change_24h"` // dynamic
-	PercentChange7D  float64 `json:"percent_change_7d"`  // dynamic
+	Price             float64 `json:"price"`
+	Volume24H         float64 `json:"volume_24h"`
+	Volume24HReported float64 `json:"volume_24h_reported"`
+	VolumeChange24H   float64 `json:"volume_change_24h"`
+	PercentChange1H   float64 `json:"percent_change_1h"`
 }
