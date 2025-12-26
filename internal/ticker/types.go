837fd5dcbe4e4fb1f9bcce4ea5b57e86789eedff
Julien Boudreau <juliendb.dev@gmail.com>
2025-12-25 20:27:16 -0400

    refactored ticker/types.go to fix the location of some fields

--- internal/ticker/types.go
+++ internal/ticker/types.go
@@ -1,7 +1,7 @@
 package ticker
 
-// All JSON fields that can be null in CMC response are pointers allowing null values to avoid
-//unmarshalling errors or setting zero values instead of nil.
+// All JSON fields that can be null in CMC API response are pointers allowing null values to avoid
+// unmarshalling errors or setting zero values instead of nil.
 // Always check documentation when adding new fields.
 // Always check for nil if trying to dereference a pointer to avoid runtime errors (panic).
 
@@ -23,32 +23,35 @@ type Status struct {
 
 // CoinInfo holds the coin related information from CMC API, including CoinQuote data.
 type CoinInfo struct {
-	ID                int     `json:"id"`
-	Name              string  `json:"name"`
-	Symbol            string  `json:"symbol"`
-	Slug              string  `json:"slug"`
-	CirculatingSupply float64 `json:"circulating_supply"`
-	TotalSupply       float64 `json:"total_supply"`
-	InfiniteSupply    bool    `json:"infinite_supply"`
-	MarketCap         float64 `json:"market_cap"`
-	// SelfReportedCirculatingSupply *float64 `json:"self_reported_circulating_supply"`
-	// SelfReportedMarketCap *float64 `json:"self_reported_market_cap"`
-	// TvlRatio *float64 `json:"tvl_ratio"`
-	// Tvl *float64 `json:"tvl"`
-	PercentChange24h float64              `json:"percent_change_24h"`
-	PercentChange7d  float64              `json:"percent_change_7d"`
-	PercentChange30d float64              `json:"percent_change_30d"`
-	PercentChange60d float64              `json:"percent_change_60d"`
-	PercentChange90d float64              `json:"percent_change_90d"`
-	LastUpdated      string               `json:"last_updated"`
-	Quote            map[string]CoinQuote `json:"quote"`
+	ID                            int      `json:"id"`
+	Name                          string   `json:"name"`
+	Symbol                        string   `json:"symbol"`
+	Slug                          string   `json:"slug"`
+	CirculatingSupply             float64  `json:"circulating_supply"`
+	TotalSupply                   float64  `json:"total_supply"`
+	InfiniteSupply                bool     `json:"infinite_supply"`
+	SelfReportedCirculatingSupply *float64 `json:"self_reported_circulating_supply"`
+	SelfReportedMarketCap         *float64 `json:"self_reported_market_cap"`
+	TvlRatio                      *float64 `json:"tvl_ratio"`
+
+	LastUpdated string               `json:"last_updated"`
+	Quote       map[string]CoinQuote `json:"quote"`
 }
 
 // CoinQuote holds the quote data for a coin from CMC API. Only uses USD for map key in CoinInfo.
 type CoinQuote struct {
-	Price             float64 `json:"price"`
-	Volume24H         float64 `json:"volume_24h"`
-	Volume24HReported float64 `json:"volume_24h_reported"`
-	VolumeChange24H   float64 `json:"volume_change_24h"`
-	PercentChange1H   float64 `json:"percent_change_1h"`
+	Price                 float64  `json:"price"`
+	MarketCap             float64  `json:"market_cap"`
+	FullyDilutedMarketCap float64  `json:"fully_diluted_market_cap"`
+	Volume24H             float64  `json:"volume_24h"`
+	Volume24HReported     float64  `json:"volume_24h_reported"`
+	VolumeChange24H       float64  `json:"volume_change_24h"`
+	PercentChange1H       float64  `json:"percent_change_1h"`
+	PercentChange24h      float64  `json:"percent_change_24h"`
+	PercentChange7d       float64  `json:"percent_change_7d"`
+	PercentChange30d      float64  `json:"percent_change_30d"`
+	PercentChange60d      float64  `json:"percent_change_60d"`
+	PercentChange90d      float64  `json:"percent_change_90d"`
+	Tvl                   *float64 `json:"tvl"`
+	LastUpdated           string   `json:"last_updated"`
 }
